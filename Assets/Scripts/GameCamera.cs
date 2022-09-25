69afa60704c1482cc8310bb88b3779e4059877f8
TotoRizzi <83927597+TotoRizzi@users.noreply.github.com>
2022-09-25 19:34:09 -0300

    Camera
    a

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    Transform _target;
+    [SerializeField] float cameraSpeed;
+    [SerializeField] Vector3 offset;
+
+    private void Start()
+    {
+        _target = GameObject.Find("Player").transform;
+    }
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = _target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, cameraSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
