b64f4f6dc35b70e40a1374fc1fa241b847ddfed6
santiago Koroch <sankoroch@gmail.com>
2022-09-25 21:47:29 -0300

    cambios escena, ambiente

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class GameCamera : MonoBehaviour
 {
-    Transform _target;
+    [SerializeField] Transform _target;
     [SerializeField] float cameraSpeed;
     [SerializeField] Vector3 offset;
 
-    private void Start()
-    {
-        _target = GameObject.Find("CameraLookAt").transform;
-    }
     private void LateUpdate()
     {
         Vector3 desiredPosition = _target.position + offset;
