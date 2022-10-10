3b8db02cc6ade38bd6ea63d64aafc3c853d7c7e4
TotoRizzi <83927597+TotoRizzi@users.noreply.github.com>
2022-10-10 18:38:12 -0300

    Platform

--- Assets/Scripts/interactableObjects/DisappearingPlatform.cs
+++ Assets/Scripts/interactableObjects/DisappearingPlatform.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class DisappearingPlatform : Platform
 {
     [SerializeField] float _timeToDestroy = 1;
+    [SerializeField] float _timeToAppear = 1;
     float _currentTimeToDestroy = 0;
+    float _currentTimeToAppear = 0;
 
+    bool _isInCoroutine = false;
+
+    [SerializeField] GameObject _myPlatform;
+    Collider _myCollider;
+
+    private void Start()
+    {
+        _myCollider = GetComponent<BoxCollider>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        StartCoroutine(DisappearPlatform());
+        Debug.Log("Colisione");
+        if (!_isInCoroutine)
+        {
+            StartCoroutine(DisappearPlatform());
+            _isInCoroutine = true;
+        }
     }
     IEnumerator DisappearPlatform()
     {
@@ -18,7 +34,27 @@ public class DisappearingPlatform : Platform
             _currentTimeToDestroy += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
-        Destroy(gameObject);
+
+        _currentTimeToDestroy = 0;
+        _myCollider.enabled = false;
+        _myPlatform.SetActive(false);
+        StartCoroutine(AppearPlatform());
+        
+        yield return null;
+    }
+    IEnumerator AppearPlatform()
+    {
+        while (_currentTimeToAppear < _timeToAppear)
+        {
+            _currentTimeToAppear += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        _currentTimeToAppear = 0;
+         _isInCoroutine = false;
+        _myPlatform.SetActive(true);
+        _myCollider.enabled = true;
+        
         yield return null;
     }
 }
